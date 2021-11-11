8d194d7d4c249e8a09c95c515dca3473a9b14b9f
dharmesh <dharmesh.chauhan@yahoo.com>
2021-11-11 05:36:44 +0530

    Ignore Pod folder in git

--- Example/Pods/Target Support Files/Pods-iRecordView_Example/Pods-iRecordView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iRecordView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_iRecordView_Example
-@end
--- Example/Pods/Target Support Files/iRecordView/iRecordView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_iRecordView : NSObject
-@end
-@implementation PodsDummy_iRecordView
-@end
